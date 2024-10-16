6d9e662264e608ca3fedc3c5d9485970aa5ab8a6
Oliver Filla <ocl@hotmail.de>
2024-10-16 13:27:58 +0200

    Script: convert md to pdf

---
+++ ASP/convert_to_pdf.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+in_file="$1"
+out_file="${2:-${1%md}pdf}"
+
+echo "convert $in_file to $out_file"
+
+file="$(cat <<EOF
+---
+header-includes:
+  - \\usepackage{amsmath}
+  - \\usepackage{physics}
+---
+$(cat "$in_file")
+EOF
+)"
+
+echo "$file" | sed -e 's/eqnarray/eqnarray*/g' -e 's/^\$\$$//g' | pandoc -o "$out_file"
